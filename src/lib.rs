3ced62431bf49da2b1e77ce9065410a22f20bb2a
Jonathan Behrens <fintelia@gmail.com>
2018-03-12 19:35:05 -0400

    Import HashMap code from standard library

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#![feature(allocator_api)]
+#![feature(dropck_eyepatch)]
+#![feature(generic_param_attrs)]
+#![feature(placement_in_syntax)]
+#![feature(placement_new_protocol)]
+#![feature(ptr_internals)]
+#![feature(sip_hash_13)]
+
+extern crate rand;
+
+mod table;
+pub mod map;
+
+trait Recover<Q: ?Sized> {
+    type Key;
+
+    fn get(&self, key: &Q) -> Option<&Self::Key>;
+    fn take(&mut self, key: &Q) -> Option<Self::Key>;
+    fn replace(&mut self, key: Self::Key) -> Option<Self::Key>;
+}
