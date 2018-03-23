8ae17deddd578328ea7b3a567d0ff858aa80694f
Jonathan Behrens <fintelia@gmail.com>
2018-03-23 17:15:15 -0400

    Add feature hashmap_internals required by newer nightlies

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(allocator_api)]
 #![feature(dropck_eyepatch)]
 #![feature(generic_param_attrs)]
+#![feature(hashmap_internals)]
 #![feature(placement_in_syntax)]
 #![feature(placement_new_protocol)]
 #![feature(ptr_internals)]
