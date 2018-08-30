f551a3c5b4685702a0096fbc981b7b5f15cb9e3c
Jonathan Behrens <fintelia@gmail.com>
2018-08-30 17:32:56 +0100

    Remove several unused features

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 #![feature(allocator_api)]
 #![feature(dropck_eyepatch)]
 #![feature(hashmap_internals)]
-#![feature(nonnull_cast)]
-#![feature(placement_in_syntax)]
-#![feature(placement_new_protocol)]
 #![feature(ptr_internals)]
-#![feature(sip_hash_13)]
 #![feature(test)]
 #![feature(try_reserve)]
 
